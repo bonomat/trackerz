142803acc6e229d19249c2fc4bee2937b3c4246d
Philipp Hoenisch <philipp@hoenisch.at>
2020-08-23 17:17:12 +1000

    A bit of cleanup

--- src/components/table.rs
+++ src/components/table.rs
@@ -211,10 +211,10 @@ impl Component for Table {
                     debug!("adding: {:?}", &file_name);
                     let js_value = if file_name.contains(".kml") {
                         url.path_segments_mut()
-                        .unwrap()
-                        .push("trackz")
-                        .push("kml")
-                        .push(&file_name);
+                            .unwrap()
+                            .push("trackz")
+                            .push("kml")
+                            .push(&file_name);
                         let url = url.as_str();
                         read_kml(url).await
                     } else {
